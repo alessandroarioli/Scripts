2421dbdeb64b7b20ac3eb3f0b50ec8e44ae68a77
root <root@localhost.localdomain>
2016-07-15 17:54:10 +0000

    Add firefox install shell script

---
+++ FirefoxInstall.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# My first shell-script
+
+echo "Start installing Firefox!"
+apt-get remove iceweasel
+echo -e "\ndeb http://downloads.sourceforge.net/project/ubuntuzilla/mozilla/apt all main" | tee -a /etc/apt/sources.list > /dev/null
+apt-key adv --recv-keys --keyserver keyserver.ubuntu.com C1289A29
+apt-get update
+apt-get install firefox-mozilla-build
+echo "Done! :)"
